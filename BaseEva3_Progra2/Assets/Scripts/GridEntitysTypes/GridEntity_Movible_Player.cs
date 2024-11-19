928995d92136f9acf18a31b3fdc94ce0460275d3
dy-0 <frutilla@tuta.io>
2024-11-18 22:53:23 -0300

    wawa

--- BaseEva3_Progra2/Assets/Scripts/GridEntitysTypes/GridEntity_Movible_Player.cs
+++ BaseEva3_Progra2/Assets/Scripts/GridEntitysTypes/GridEntity_Movible_Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GridEntity_Movible_Player : GridEntity_Movible
 {
     public Vector2Int startPos;
+    private float lastMov = 0f;
+    private float coolDown = 0.7f;
 
     protected override void Awake2()
     {
@@ -32,31 +34,36 @@ public class GridEntity_Movible_Player : GridEntity_Movible
 
     void MoveInputs()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector2Int dir = Vector2Int.zero;
+        if (Time.time - lastMov >= coolDown)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            Vector2Int dir = Vector2Int.zero;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            dir.y = 1;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            dir.y = -1;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            dir.x = -1;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            dir.x = 1;
-        }
+            if (Input.GetKey(KeyCode.W))
+            {
+                dir.y = 1;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                dir.y = -1;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                dir.x = -1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                dir.x = 1;
+            }
 
-        if (dir.magnitude != 0)
-        {
-            transform.forward = new Vector3(dir.x, 0, dir.y);
-            Move(dir);
+            if (dir.magnitude != 0)
+            {
+                transform.forward = new Vector3(dir.x, 0, dir.y);
+                Move(dir);
+
+                lastMov = Time.time;
+            }
         }
     }
 
